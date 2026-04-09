6e1bcd10ac6d6d177379133e81083e6ffe230412
Comedy-Club <alexlixiang999@gmail.com>
2026-04-08 22:31:44 -0700

    Create config.example.js

---
+++ src/config.example.js
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: 'your-api-key',
+  authDomain: 'your-project.firebaseapp.com',
+  projectId: 'your-project-id',
+  storageBucket: 'your-project.appspot.com',
+  messagingSenderId: '1234567890',
+  appId: '1:1234567890:web:abc123'
+};
